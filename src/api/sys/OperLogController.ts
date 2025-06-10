0e3f1f1f9bfabd2eed6ec6f5e2e7eff6e7f9f5fe
mikaka <826305303@qq.com>
2025-06-10 22:22:31 +0800

    feat: 操作日志

---
+++ src/api/sys/OperLogController.ts
@@ -0,0 +1,54 @@
+import { BaseController } from "@/api/BaseController";
+import { PageReq } from "@/api/sys/UserController";
+import { PageDataType } from "@/common/http/types";
+
+export default class OperLogController extends BaseController {
+  static page(req: OperLogPageReq) {
+    return super.GET<PageDataType<OperLogDTO>>("/oper-log", req);
+  }
+
+  static detail(logId: string) {
+    return super.GET<OperLogDTO>(`/oper-log/${logId}`);
+  }
+}
+
+export type OperLogPageReq = PageReq & {
+  success: boolean;
+};
+
+export type OperLogDTO = {
+  /**
+   * 日志主键
+   */
+  id: number;
+
+  /**
+   * 模块标题
+   */
+  title: string;
+
+  /**
+   * 操作用户名称
+   */
+  operUseName: string;
+
+  /**
+   * 主机地址
+   */
+  operIp: string;
+
+  /**
+   * 成功标识（1成功 0失败）
+   */
+  success: boolean;
+
+  /**
+   * 操作时间
+   */
+  operTime: string;
+
+  /**
+   * 消耗时间
+   */
+  costTime: number;
+};
--- src/app/sys/oper-log/page.tsx
+++ src/app/sys/oper-log/page.tsx
@@ -1,5 +1,154 @@
 "use client";
 
-export default function FrontPage() {
-  return <div>oper-log</div>;
+import Box from "@/components/box";
+import { Button, Col, Form, Row, Select, Space, Table, TableProps } from "antd";
+import { SearchOutlined, SyncOutlined } from "@ant-design/icons";
+import OperLogController, {
+  OperLogDTO,
+  OperLogPageReq,
+} from "@/api/sys/OperLogController";
+import { useEffect, useState } from "react";
+import dayjs from "dayjs";
+
+const operLogSuccessOption = [
+  {
+    value: "1",
+    label: "成功",
+  },
+  {
+    value: "0",
+    label: "失败",
+  },
+];
+
+export default function OperLogPage() {
+  const [queryForm] = Form.useForm<OperLogPageReq>();
+  const [pageNum, setPageNum] = useState<number>(1);
+  const [pageSize, setPageSize] = useState<number>(10);
+  const [total, setTotal] = useState<number>(0);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [operLogs, setOperLogs] = useState<OperLogDTO[]>([]);
+
+  useEffect(() => {
+    doQuery(1);
+  }, []);
+  const reloadData = () => {
+    setPageNum(1);
+    doQuery(1);
+  };
+
+  const doQuery = async (currentPage = 1) => {
+    setLoading(true);
+    const queryParam = queryForm.getFieldsValue();
+    const pageResult = await OperLogController.page({
+      ...queryParam,
+      pageNum: currentPage,
+      pageSize,
+    });
+    setOperLogs(pageResult.records || []);
+    setTotal(pageResult.total);
+    setLoading(false);
+  };
+
+  const handlePageOnChange = (page: number, pageSize: number) => {
+    setPageNum(page);
+    setPageSize(pageSize);
+    doQuery(page);
+  };
+
+  const columns: TableProps<OperLogDTO>["columns"] = [
+    {
+      title: "模块",
+      dataIndex: "title",
+      key: "title",
+    },
+    {
+      title: "用户名称",
+      dataIndex: "operUseName",
+      key: "operUseName",
+    },
+    {
+      title: "主机地址",
+      dataIndex: "operIp",
+      key: "operIp",
+    },
+    {
+      title: "成功标识",
+      dataIndex: "success",
+      key: "success",
+      render: (_, dto) => {
+        return dto.success ? "成功" : "失败";
+      },
+    },
+    {
+      title: "消耗时间",
+      dataIndex: "costTime",
+      key: "costTime",
+    },
+    {
+      title: "操作时间",
+      dataIndex: "operTime",
+      key: "operTime",
+      render: (_, dto) => {
+        const loginDate = dayjs(dto.operTime);
+        if (!loginDate.isValid()) {
+          return "";
+        }
+        return loginDate.format("YYYY-MM-DD HH:mm:ss");
+      },
+    },
+  ];
+
+  return (
+    <>
+      <Box>
+        <Form form={queryForm} name="advanced_search">
+          <Row gutter={24}>
+            <Col span={6}>
+              <Form.Item name="success" label="状态">
+                <Select options={operLogSuccessOption}></Select>
+              </Form.Item>
+            </Col>
+          </Row>
+          <div className="text-left">
+            <Space>
+              <Button
+                type="primary"
+                htmlType="submit"
+                icon={<SearchOutlined />}
+                onClick={reloadData}
+              >
+                查询
+              </Button>
+              <Button
+                icon={<SyncOutlined />}
+                onClick={() => {
+                  queryForm.resetFields();
+                }}
+              >
+                重置
+              </Button>
+            </Space>
+          </div>
+        </Form>
+      </Box>
+
+      <div className="mt-4">
+        <Box>
+          <Table
+            dataSource={operLogs}
+            columns={columns}
+            rowKey={(record) => record.id}
+            loading={loading}
+            pagination={{
+              total,
+              pageSize,
+              current: pageNum,
+              onChange: handlePageOnChange,
+            }}
+          />
+        </Box>
+      </div>
+    </>
+  );
 }

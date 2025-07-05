739a06b24555365aa60c94f8f810d0f40217f1ad
mikaka <826305303@qq.com>
2025-07-05 16:52:07 +0800

    feat: add dict

--- src/app/(protected)/prod/dict/page.tsx
+++ src/app/(protected)/prod/dict/page.tsx
@@ -5,22 +5,31 @@ import {
   Col,
   Form,
   Input,
+  Modal,
   Radio,
   Row,
   Space,
   Table,
   TableProps,
+  Tag,
 } from "antd";
 import Box from "@/components/box";
 import { Microchip, Tags, Weight } from "lucide-react";
-import { useState } from "react";
-import { PlusOutlined, SearchOutlined, SyncOutlined } from "@ant-design/icons";
+import { useRef, useState } from "react";
+import {
+  CheckOutlined,
+  CloseOutlined,
+  DeleteOutlined,
+  PlusOutlined,
+  SearchOutlined,
+  SyncOutlined,
+} from "@ant-design/icons";
 import DictController, {
+  DictDTO,
   DictGroupDTO,
   DictQueryReq,
   DictType,
 } from "@/api/prod/DictController";
-import RoleController from "@/api/sys/RoleController";
 
 const options = Object.entries(DictType).map(([value, label]) => ({
   value,
@@ -36,15 +45,23 @@ const options = Object.entries(DictType).map(([value, label]) => ({
 }));
 
 export default function DictPage() {
-  const [dictType, setDictType] = useState<string>();
+  const [dictType, setDictType] = useState<string>("UNIT");
   const [queryForm] = Form.useForm<DictQueryReq>();
   const [loading, setLoading] = useState<boolean>(false);
   const [dictGroupList, setDictGroupList] = useState<DictGroupDTO[]>();
   const [currentDictGroup, setCurrentDictGroup] = useState<DictGroupDTO>();
+  const [dictGroupForm] = Form.useForm<DictGroupDTO>();
+  const [modalVisible, setModalVisible] = useState<boolean>(false);
+  const [detailModalVisible, setDetailModalVisible] = useState<boolean>(false);
+  const [currentDictDetail, setCurrentDictDetail] = useState<DictDTO>();
+  const [dictDetailForm] = Form.useForm<DictDTO>();
 
-  const doQuery = async () => {
+  const [tagInputVisible, setTagInputVisible] = useState(false);
+  const [tagInputValue, setTagInputValue] = useState("");
+  const tagInputRef = useRef<string>("");
+
+  const doQuery = async (dictType: string) => {
     setLoading(true);
-
     const queryParam = queryForm.getFieldsValue();
     queryParam.type = dictType;
     const result = await DictController.list(queryParam);
@@ -53,30 +70,84 @@ export default function DictPage() {
   };
 
   const handleEdit = async (dictGroup: DictGroupDTO) => {
-    await DictController.edit(dictGroup.dictGroupId, dictGroup);
+    setCurrentDictGroup(dictGroup);
+    dictGroupForm.setFieldsValue(dictGroup);
+    setModalVisible(true);
   };
 
   const handleCreate = async () => {
-    // const dictGroup = await DictController.create(currentDictGroup);
-    // setCurrentDictGroup(dictGroup);
-    // setModalDetailVisible(true);
+    setCurrentDictGroup(undefined);
+    dictGroupForm.resetFields();
+    setModalVisible(true);
   };
+
+  const handleSubmit = async () => {
+    const values = await dictGroupForm.validateFields();
+    values.type = dictType;
+    if (currentDictGroup) {
+      await DictController.edit(currentDictGroup.dictGroupId, values);
+    } else {
+      await DictController.create(values);
+    }
+    setModalVisible(false);
+    dictGroupForm.resetFields();
+    doQuery(dictType);
+  };
+
+  const handleDetailSubmit = async () => {
+    const dictDetail = await dictDetailForm.validateFields();
+    dictDetail.type = dictType;
+    if (currentDictDetail) {
+      await DictController.editDetail(
+        currentDictGroup!.dictGroupId,
+        currentDictDetail.dictId,
+        dictDetail,
+      );
+    } else {
+      await DictController.createDetail(
+        currentDictGroup!.dictGroupId,
+        dictDetail,
+      );
+    }
+    setDetailModalVisible(false);
+    dictDetailForm.resetFields();
+    doQuery(dictType);
+  };
+
+  const dictTypeName = DictType[dictType as keyof typeof DictType];
 
   const columns: TableProps<DictGroupDTO>["columns"] = [
     {
-      title: "字典组名称",
+      title: `${dictTypeName}名称`,
       dataIndex: "name",
       key: "name",
     },
     {
-      title: "子字典列表",
+      title: `${dictTypeName}明细`,
       dataIndex: "dictDetails",
       key: "dictDetails",
       render: (_, dictGroup) => (
         <>
-          {dictGroup.dictDetails.map((dict) => (
-            <div key={dict.dictId}>{dict.name}</div>
-          ))}
+          {dictGroup.dictDetails &&
+            dictGroup.dictDetails.map((dict) => (
+              <Tag
+                key={dict.dictId}
+                closable={{
+                  closeIcon: <DeleteOutlined />,
+                  "aria-label": "Close Button",
+                }}
+                onClose={async (e) => {
+                  e.preventDefault();
+                  await DictController.deleteDetail(
+                    dictGroup.dictGroupId,
+                    dict.dictId,
+                  );
+                  doQuery(dictType);
+                }}
+              >
+                {dict.name}
+              </Tag>
+            ))}
         </>
       ),
     },
@@ -84,17 +155,6 @@ export default function DictPage() {
       title: "操作",
       render: (_, dictGroup) => (
         <>
-          <Button
-            type="link"
-            size="small"
-            onClick={async () => {
-              const data = await DictController.detail(dictGroup.dictGroupId);
-              setCurrentDictGroup(data);
-              // setModalDetailVisible(true);
-            }}
-          >
-            详情
-          </Button>
           <Button
             type="link"
             size="small"
@@ -106,11 +166,22 @@ export default function DictPage() {
             type="link"
             size="small"
             onClick={async () => {
-              await RoleController.delete(dictGroup.dictGroupId);
-              doQuery();
+              await DictController.delete(dictGroup.dictGroupId);
+              doQuery(dictType);
             }}
           >
             删除
+          </Button>
+          <Button
+            type="link"
+            size="small"
+            onClick={async () => {
+              setCurrentDictGroup(dictGroup);
+              setCurrentDictDetail(undefined);
+              setDetailModalVisible(true);
+            }}
+          >
+            添加{dictTypeName}明细
           </Button>
         </>
       ),
@@ -126,67 +197,161 @@ export default function DictPage() {
           defaultValue="UNIT"
           optionType="button"
           buttonStyle="solid"
-          onChange={(e) => setDictType(e.target.value)}
+          onChange={(e) => {
+            setDictType(e.target.value);
+            doQuery(e.target.value);
+          }}
         />
       </Box>
-      {dictType == "UNIT" && <Box>单位</Box>}
-      {dictType == "LABEL" && <Box>标签</Box>}
-      {dictType == "SPEC" && (
-        <Box>
-          {/*查询窗口*/}
-          <Form form={queryForm} name="advanced_search">
-            <Row gutter={24}>
-              <Col span={6}>
-                <Form.Item name="name" label="名称">
-                  <Input placeholder="请输入名称" />
-                </Form.Item>
-              </Col>
-              <div className="text-left">
-                <Space>
-                  <Button
-                    type="primary"
-                    htmlType="submit"
-                    icon={<SearchOutlined />}
-                    onClick={doQuery}
-                  >
-                    查询
-                  </Button>
-                  <Button
-                    icon={<SyncOutlined />}
-                    onClick={() => {
-                      queryForm.resetFields();
-                    }}
-                  >
-                    重置
-                  </Button>
-                </Space>
-              </div>
-            </Row>
-          </Form>
 
-          {/*数据列表*/}
-          <Space className="pb-4">
-            <Button
-              type="primary"
-              icon={<PlusOutlined />}
-              onClick={handleCreate}
-            >
-              创建
-            </Button>
+      <Box>
+        {/*查询窗口*/}
+        <Form form={queryForm} name="advanced_search">
+          <Row gutter={24}>
+            <Col span={6}>
+              <Form.Item name="name" label={`${dictTypeName}名称`}>
+                <Input placeholder={`请输入${dictTypeName}名称`} />
+              </Form.Item>
+            </Col>
+            <div className="text-left">
+              <Space>
+                <Button
+                  type="primary"
+                  htmlType="submit"
+                  icon={<SearchOutlined />}
+                  onClick={() => doQuery(dictType)}
+                >
+                  查询
+                </Button>
+                <Button
+                  icon={<SyncOutlined />}
+                  onClick={() => {
+                    queryForm.resetFields();
+                  }}
+                >
+                  重置
+                </Button>
+              </Space>
+            </div>
+          </Row>
+        </Form>
 
-            <Button type="primary" icon={<SyncOutlined />} onClick={doQuery}>
-              刷新
-            </Button>
-          </Space>
-          <Table
-            dataSource={dictGroupList}
-            columns={columns}
-            rowKey={(record) => record.dictGroupId}
-            loading={loading}
-            pagination={false}
-          />
-        </Box>
-      )}
+        {/*数据列表*/}
+        <Space className="pb-4">
+          <Button type="primary" icon={<PlusOutlined />} onClick={handleCreate}>
+            创建
+          </Button>
+
+          <Button
+            type="primary"
+            icon={<SyncOutlined />}
+            onClick={() => doQuery(dictType)}
+          >
+            刷新
+          </Button>
+        </Space>
+        <Table
+          dataSource={dictGroupList}
+          columns={columns}
+          rowKey={(record) => record.dictGroupId}
+          loading={loading}
+          pagination={false}
+        />
+      </Box>
+
+      {/*====> 弹窗 begin*/}
+      {/*字典组弹窗*/}
+      <Modal
+        title={
+          currentDictGroup ? `编辑${dictTypeName}组` : `创建${dictTypeName}组`
+        }
+        open={modalVisible}
+        onOk={handleSubmit}
+        onCancel={() => {
+          setModalVisible(false);
+          dictGroupForm.resetFields();
+        }}
+        footer={null}
+      >
+        <Form form={dictGroupForm} layout="vertical" name="deviceForm">
+          <Form.Item
+            name="name"
+            label={`${dictTypeName}组名称`}
+            rules={[{ required: true, message: `请输入${dictTypeName}组名称` }]}
+          >
+            <Input placeholder={`请输入${dictTypeName}组名称`} />
+          </Form.Item>
+
+          <div className="text-right">
+            <Space>
+              <Button
+                icon={<CloseOutlined />}
+                onClick={() => {
+                  setModalVisible(false);
+                  dictGroupForm.resetFields();
+                }}
+              >
+                取消
+              </Button>
+              <Button
+                type="primary"
+                htmlType="submit"
+                icon={<CheckOutlined />}
+                onClick={handleSubmit}
+              >
+                确认
+              </Button>
+            </Space>
+          </div>
+        </Form>
+      </Modal>
+
+      {/*字典详情弹窗*/}
+      <Modal
+        title={
+          currentDictDetail ? `编辑${dictTypeName}` : `创建${dictTypeName}`
+        }
+        open={detailModalVisible}
+        onOk={handleDetailSubmit}
+        onCancel={() => {
+          setDetailModalVisible(false);
+          dictDetailForm.resetFields();
+        }}
+        footer={null}
+      >
+        <Form form={dictDetailForm} layout="vertical" name="deviceForm">
+          <Form.Item
+            name="name"
+            label={`${dictTypeName}名称`}
+            rules={[{ required: true, message: `请输入${dictTypeName}名称` }]}
+          >
+            <Input placeholder={`请输入${dictTypeName}名称`} />
+          </Form.Item>
+
+          <div className="text-right">
+            <Space>
+              <Button
+                icon={<CloseOutlined />}
+                onClick={() => {
+                  setDetailModalVisible(false);
+                  dictDetailForm.resetFields();
+                }}
+              >
+                取消
+              </Button>
+              <Button
+                type="primary"
+                htmlType="submit"
+                icon={<CheckOutlined />}
+                onClick={handleDetailSubmit}
+              >
+                确认
+              </Button>
+            </Space>
+          </div>
+        </Form>
+      </Modal>
+      {/*====> 弹窗 end*/}
     </>
   );
 }

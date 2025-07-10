e1be770bf2121f2d355d9b34b4563a7365aae224
mikaka <826305303@qq.com>
2025-07-11 00:11:57 +0800

    feat: sku

--- src/app/(protected)/prod/spu/edit/page.tsx
+++ src/app/(protected)/prod/spu/edit/page.tsx
@@ -3,20 +3,18 @@
 import { CheckOutlined, CloseOutlined, PlusOutlined } from "@ant-design/icons";
 import {
   Button,
-  Card,
   Cascader,
-  Col,
   Form,
   Input,
-  Row,
   Select,
   Space,
   Switch,
+  Table,
+  TableProps,
   Typography,
   Upload,
 } from "antd";
-import { useParams, useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import Box from "@/components/box";
 import DictController, {
   DictDTO,
@@ -29,30 +27,41 @@ import SpuController, {
   SkuDTO,
   SpuDTO,
 } from "@/api/prod/SpuController";
+import { useRouter, useSearchParams } from "next/navigation";
 
 const { TextArea } = Input;
 
+type SkuProp = {
+  key: number;
+  price?: number;
+  stock?: number;
+  specs?: DictDTO[];
+};
+
 export default function SpuEditPage() {
-  const { spuId } = useParams();
-
-  const [currentSpu, setCurrentSpu] = useState<SpuDTO>();
-  if (spuId) {
-    SpuController.detail(spuId as string).then((res) => {
-      setCurrentSpu(res);
-    });
-  }
-
-  const [unitList, setUnitList] = useState<DictDTO[]>([]);
-  const [specList, setSpecList] = useState<DictGroupDTO[]>([]);
-  const [categoryList, setCategoryList] = useState<CategoryDTO[]>([]);
-  const [spuForm] = Form.useForm<SpuDTO>();
   const router = useRouter();
+  const searchParams = useSearchParams();
+  const spuId = searchParams.get("spuId");
+  const [currentSpu, setCurrentSpu] = useState<SpuDTO>();
 
-  // 属性
+  // 下拉属性
+  const [unitList, setUnitList] = useState<DictDTO[]>([]);
+  const [categoryList, setCategoryList] = useState<CategoryDTO[]>([]);
+
+  // From表单外属性
   const [categoryId, setCategoryId] = useState<string>("");
   const [spuStatus, setSpuStatus] = useState<string>(ProdStatusEnum.ENABLE);
   // const [imgUrlList, setImgUrlList] = useState<string[]>([]);
 
+  // sku属性，计算属性
+  const [dictGroupList, setDictGroupList] = useState<DictGroupDTO[]>([]);
+  const [allDictGroupList, setAllDictGroupList] = useState<DictGroupDTO[]>([]);
+  const [skuSpecList, setSkuSpecList] = useState<SkuProp[]>([]);
+  const [columns, setColumns] = useState<TableProps<SkuProp>["columns"]>();
+
+  // 表单
+  const [spuForm] = Form.useForm<SpuDTO>();
+  // 初始化单位、规格、分类
   useEffect(() => {
     DictController.list({ type: "UNIT" }).then((dictGroupList) => {
       if (dictGroupList) {
@@ -62,7 +71,7 @@ export default function SpuEditPage() {
 
     DictController.list({ type: "SPEC" }).then((dictGroupList) => {
       if (dictGroupList) {
-        setSpecList(dictGroupList);
+        setAllDictGroupList(dictGroupList);
       }
     });
 
@@ -71,7 +80,142 @@ export default function SpuEditPage() {
         setCategoryList(categoryList);
       }
     });
+
+    if (spuId) {
+      SpuController.detail(spuId as string).then((res) => {
+        setCurrentSpu(res);
+        spuForm.setFieldsValue(res);
+      });
+    }
   }, []);
+
+  // 计算可能的sku
+  useEffect(() => {
+    const selectedDetails = dictGroupList
+      .map((group) => group.dictDetails.filter((detail) => detail.checked))
+      .filter((details) => details.length > 0);
+
+    if (selectedDetails.length === 0) {
+      setSkuSpecList([]);
+      return;
+    }
+
+    // 计算笛卡尔积
+    const cartesian = selectedDetails.reduce<DictDTO[][]>(
+      (acc, curr) => curr.flatMap((item) => acc.map((prev) => [...prev, item])),
+      [[]],
+    );
+
+    // 转换
+    const formattedList = cartesian.map((combination, index) => {
+      const skuItem = combination.reduce<DictDTO[]>((acc, item) => {
+        return [...acc, item];
+      }, []);
+
+      return {
+        key: index,
+        specs: skuItem,
+      } as SkuProp;
+    });
+    setSkuSpecList(formattedList);
+
+    if (dictGroupList.length === 0 || !skuSpecList.length) {
+      setColumns([]);
+    } else {
+      const col = dictGroupList.map((group, index) => ({
+        title: group.name,
+        dataIndex: `spec_${index}`,
+        key: index,
+        width: 100,
+        render: (_: unknown, record: SkuProp) => {
+          const spec = record!.specs![index];
+          return <>{spec?.name ?? "-"}</>;
+        },
+      }));
+      col.push({
+        title: "价格",
+        dataIndex: "price",
+        key: 100,
+        width: 200,
+        render: (_: unknown, record: SkuProp) => {
+          return (
+            <>
+              <Input
+                type="number"
+                step="0.1"
+                prefix="￥"
+                suffix="元"
+                required
+                value={Number(record.price)}
+                onChange={(e) =>
+                  setSkuSpecList((prev) => {
+                    return prev.map((item) =>
+                      item.key === record.key
+                        ? ({
+                            ...item,
+                            price: Number(e.target.value),
+                          } as SkuProp)
+                        : item,
+                    );
+                  })
+                }
+              />
+            </>
+          );
+        },
+      });
+      setColumns(col);
+    }
+  }, [dictGroupList]);
+
+  const property = {
+    add(dictGroup: DictGroupDTO) {
+      setDictGroupList((prev) => [...prev, dictGroup]);
+    },
+    remove(index: number) {
+      setDictGroupList((prev) => prev.filter((_, i) => i !== index));
+    },
+    change(i: number, dictGroupId: string) {
+      const dictGroup = allDictGroupList.find(
+        (item) => item.dictGroupId === dictGroupId,
+      );
+      if (dictGroup) {
+        setDictGroupList((prev) => {
+          // 检查是否已经存在该 dictGroup
+          const exists = prev.some(
+            (group) => group.dictGroupId === dictGroup.dictGroupId,
+          );
+
+          if (exists) {
+            return prev; // 已存在，不更新
+          }
+
+          const updatedList = [...prev];
+          updatedList[i] = dictGroup;
+          return updatedList;
+        });
+      }
+    },
+    onChangeDetail(i: number, dictIds: string[]) {
+      setDictGroupList((prev) => {
+        const updatedList = [...prev];
+
+        if (i >= 0 && i < updatedList.length) {
+          const currentGroup = { ...updatedList[i] };
+
+          // 更新 dictDetails 中每个项的 checked 状态
+          currentGroup.dictDetails = currentGroup.dictDetails.map((detail) => ({
+            ...detail,
+            checked: dictIds.includes(detail.dictId),
+          }));
+
+          updatedList[i] = currentGroup;
+        }
+
+        return updatedList;
+      });
+    },
+  };
 
   const unitSelectOptions = unitList.map((dict) => ({
     value: dict.dictId,
@@ -87,10 +231,6 @@ export default function SpuEditPage() {
     "value",
     "parent",
   );
-
-  const handleCancel = () => {
-    router.back();
-  };
 
   const handleSubmit = () => {
     spuForm.validateFields().then(async (spuDTO) => {
@@ -190,101 +330,74 @@ export default function SpuEditPage() {
         {/*------------------ 规格 begin ------------------*/}
         <div className={"text-sm font-medium mb-6 "}>价格信息</div>
 
-        <Row>
-          <Col offset={4} span={20}>
-            <Form.List name="items">
-              {(fields, { add, remove }) => (
-                <div
-                  style={{
-                    display: "flex",
-                    rowGap: 16,
-                    flexDirection: "column",
+        <Form.Item label="商品规格">
+          <Space direction="vertical">
+            {dictGroupList.map((dictGroup, i) => (
+              <Space key={i}>
+                <Select
+                  placeholder="规格名"
+                  style={{ minWidth: 200 }}
+                  options={allDictGroupList.map((item) => {
+                    return { value: item.dictGroupId, label: item.name };
+                  })}
+                  value={dictGroup.dictGroupId}
+                  onChange={(dictGroupId: string) =>
+                    property.change(i, dictGroupId)
+                  }
+                />
+                <Select
+                  mode="multiple"
+                  allowClear
+                  style={{ minWidth: 400 }}
+                  placeholder="选择规格属性"
+                  onChange={(dictIds: string[]) => {
+                    property.onChangeDetail(i, dictIds);
+                  }}
+                  value={dictGroup.dictDetails
+                    .filter((detail) => detail.checked)
+                    .map((detail) => detail.dictId)}
+                  options={dictGroup.dictDetails.map((detail) => {
+                    return { label: detail.name, value: detail.dictId };
+                  })}
+                />
+                <Button
+                  type="link"
+                  danger
+                  onClick={() => {
+                    property.remove(i);
                   }}
                 >
-                  {fields.map((field) => (
-                    <Card
-                      size="small"
-                      title={`Item ${field.name + 1}`}
-                      key={field.key}
-                      extra={
-                        <CloseOutlined
-                          onClick={() => {
-                            remove(field.name);
-                          }}
-                        />
-                      }
-                    >
-                      <Form.Item label="Name" name={[field.name, "name"]}>
-                        <Select
-                          options={specList.map((item) => ({
-                            value: item.dictGroupId,
-                            label: item.name,
-                          }))}
-                        />
-                      </Form.Item>
+                  删除
+                </Button>
+              </Space>
+            ))}
+            {dictGroupList.length < 3 && (
+              <Button
+                type="dashed"
+                size={"small"}
+                onClick={() => {
+                  property.add({
+                    dictGroupId: "",
+                    name: "",
+                    dictDetails: [],
+                  } as DictGroupDTO);
+                }}
+              >
+                添加规格
+              </Button>
+            )}
+          </Space>
+        </Form.Item>
 
-                      {/* Nest Form.List */}
-                      <Form.Item label="List">
-                        <Form.List name={[field.name, "list"]}>
-                          {(subFields, subOpt) => (
-                            <div
-                              style={{
-                                display: "flex",
-                                flexDirection: "column",
-                                rowGap: 16,
-                              }}
-                            >
-                              {subFields.map((subField) => (
-                                <Space key={subField.key}>
-                                  <Form.Item
-                                    noStyle
-                                    name={[subField.name, "first"]}
-                                  >
-                                    <Input placeholder="first" />
-                                  </Form.Item>
-                                  <Form.Item
-                                    noStyle
-                                    name={[subField.name, "second"]}
-                                  >
-                                    <Input placeholder="second" />
-                                  </Form.Item>
-                                  <CloseOutlined
-                                    onClick={() => {
-                                      subOpt.remove(subField.name);
-                                    }}
-                                  />
-                                </Space>
-                              ))}
-                              <Button
-                                type="dashed"
-                                onClick={() => subOpt.add()}
-                                block
-                              >
-                                + 添加规格明细
-                              </Button>
-                            </div>
-                          )}
-                        </Form.List>
-                      </Form.Item>
-                    </Card>
-                  ))}
-
-                  <Button type="dashed" onClick={() => add()} block>
-                    + 添加规格
-                  </Button>
-                </div>
-              )}
-            </Form.List>
-
-            <Form.Item noStyle shouldUpdate>
-              {() => (
-                <Typography>
-                  <pre>{JSON.stringify(spuForm.getFieldsValue(), null, 2)}</pre>
-                </Typography>
-              )}
-            </Form.Item>
-          </Col>
-        </Row>
+        <Form.Item label="规格明细" wrapperCol={{ span: 16 }}>
+          <Table
+            pagination={false}
+            size="small"
+            columns={columns}
+            dataSource={skuSpecList}
+            bordered
+          />
+        </Form.Item>
 
         {/* ------------------ 规格 end ------------------*/}
 
@@ -321,8 +434,23 @@ export default function SpuEditPage() {
           <TextArea rows={3} />
         </Form.Item>
 
+        <Form.Item label="调试信息" shouldUpdate>
+          {() => (
+            <Typography>
+              <pre>{JSON.stringify(spuForm.getFieldsValue(), null, 2)}</pre>
+
+              <pre>{JSON.stringify(skuSpecList, null, 2)}</pre>
+            </Typography>
+          )}
+        </Form.Item>
+
         <Space className={"w-full justify-center py-4"}>
-          <Button icon={<CloseOutlined />} onClick={handleCancel}>
+          <Button
+            icon={<CloseOutlined />}
+            onClick={() => {
+              router.back();
+            }}
+          >
             取消
           </Button>
           <Button
